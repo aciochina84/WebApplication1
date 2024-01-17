c962f96c690fb95eac16533a146a2f2b535cb568
Andrei Ciochina <a.ciochina@gmail.com>
2024-01-17 16:24:51 +0100

    C# GetArticolo Example

---
+++ WebApplication1/Classi/Articolo.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication1.Classi
+{
+    public class Articolo
+    {
+        public Int32 ArticoloID { get; set; }
+        // Nome? potrebbe essere null
+        public String? Nome { get; set; }
+        public String? Descrizione { get; set; }
+        public Single PrezzoUnitarioVendita { get; set; }
+        public Int32 Giacenza { get; set; }
+        public Int32 AliquotaIVA { get; set; }
+
+        public Articolo()
+        {
+            
+        }
+    }
+}
---
+++ WebApplication1/Controllers/ArticoliController.cs
@@ -0,0 +1,249 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using WebApplication1.Classi;
+
+namespace WebApplication1.Controllers
+{
+    [Route("articoli")]
+    [ApiController]
+    public class ArticoliController : ControllerBase
+    {
+        // qua dentro dobbiamo programmare le chiamate
+        // che potrebbero essere decine...
+        // come prima chiamata faremo un - GET - che restituisce
+        // tutti gli articoli
+
+        [Route("listaarticoli")]
+        [HttpGet]
+        public List<Articolo> GetListArticoli()
+        {
+            SqlConnection mySqlConnection = null;
+
+            // dichiaro una List di Articoli
+            List<Articolo> myListArticoli = new List<Articolo>();
+
+            try
+            {
+                // per connettermi al db
+                // mi serve una stringa di connessione
+                String stringaConnessione = "Server=localhost\\SQLEXPRESS; Database=DBGestionaleVI; Trusted_Connection=True";
+
+                // creo l'istanza della connessione al DB
+                mySqlConnection = new SqlConnection(stringaConnessione);
+
+                // provo aprire la connessione al DB
+                mySqlConnection.Open();
+
+                // creo l'oggetto che mi permette di estrarre i dati da TArticoli
+                SqlCommand mySqlCommand = new SqlCommand();
+                mySqlCommand.Connection = mySqlConnection;
+                mySqlCommand.CommandText = "SELECT * FROM TArticoli";
+
+                // devo eseguire il mySqlCommand e ottengo un SqlDataReader
+                SqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
+
+                // adesso il mySqlDataReader contiene i vari record
+                // che andranno copiati dentro myListArticoli
+
+                while (mySqlDataReader.Read())
+                {
+                    // leggo un record alla volta
+                    // se sono qua significa che ho trovato un Record in TArticoli
+                    //Articolo myArticolo = new Articolo();
+                    //// compilo myArticolo e poi l'ho aggiungo alla myListArticoli
+                    //myArticolo.ArticoloID = Convert.ToInt32(mySqlDataReader["ArticoloID"]);
+                    //myArticolo.Nome = Convert.ToString(mySqlDataReader["Nome"]);
+                    //myArticolo.Descrizione = Convert.ToString(mySqlDataReader["Descrizione"]);
+                    //myArticolo.PrezzoUnitarioVendita = Convert.ToSingle(mySqlDataReader["PrezzoUnitarioVendita"]);
+                    //myArticolo.Giacenza = Convert.ToInt32(mySqlDataReader["Giacenza"]);
+                    //myArticolo.AliquotaIVA = Convert.ToInt32(mySqlDataReader["AliquotaIVA"]);
+
+                    Articolo myArticolo = new Articolo
+                    {
+                        ArticoloID = Convert.ToInt32(mySqlDataReader["ArticoloID"]),
+                        Nome = Convert.ToString(mySqlDataReader["Nome"]),
+                        Descrizione = Convert.ToString(mySqlDataReader["Descrizione"]),
+                        PrezzoUnitarioVendita = Convert.ToSingle(mySqlDataReader["PrezzoUnitarioVendita"]),
+                        Giacenza = Convert.ToInt32(mySqlDataReader["Giacenza"]),
+                        AliquotaIVA = Convert.ToInt32(mySqlDataReader["AliquotaIVA"])
+                    };
+
+                    // myArticolo puo essere aggiunto alla lista degli articoli
+                    myListArticoli.Add(myArticolo);
+                }
+
+            }
+            catch (Exception exc)
+            {
+                // in caso di errore dovremo comunicare al frontend
+                // tramite un'altro metodo
+            }
+            finally
+            {
+                // che tutto sia OK oppure KO il codice dentro finally verra sempre eseguita x.y
+                // chiudo la connessione al db
+                mySqlConnection.Close();
+            }
+
+            // fine della funzione GetListArticoli
+
+            return myListArticoli;
+        }
+
+        //********************************************************
+        //          RETURN ARTICOLI CON GIACENZA ZERO
+        //********************************************************
+
+
+        [Route("listaarticoli_fuoristock")]
+        [HttpGet]
+        public List<Articolo> GetListArticoliFuoriStock()
+        {
+            SqlConnection mySqlConnection = null;
+
+            // dichiaro una List di Articoli
+            List<Articolo> myListArticoli = new List<Articolo>();
+
+            try
+            {
+                // per connettermi al db
+                // mi serve una stringa di connessione
+                String stringaConnessione = "Server=localhost\\SQLEXPRESS; Database=DBGestionaleVI; Trusted_Connection=True";
+
+                // creo l'istanza della connessione al DB
+                mySqlConnection = new SqlConnection(stringaConnessione);
+
+                // provo aprire la connessione al DB
+                mySqlConnection.Open();
+
+                // creo l'oggetto che mi permette di estrarre i dati da TArticoli
+                SqlCommand mySqlCommand = new SqlCommand();
+                mySqlCommand.Connection = mySqlConnection;
+                mySqlCommand.CommandText = "SELECT * FROM TArticoli WHERE Giacenza=0";
+
+                // devo eseguire il mySqlCommand e ottengo un SqlDataReader
+                SqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
+
+                // adesso il mySqlDataReader contiene i vari record
+                // che andranno copiati dentro myListArticoli
+
+                while (mySqlDataReader.Read())
+                {
+                    // leggo un record alla volta
+                    // se sono qua significa che ho trovato un Record in TArticoli
+                    //Articolo myArticolo = new Articolo();
+                    //// compilo myArticolo e poi l'ho aggiungo alla myListArticoli
+                    //myArticolo.ArticoloID = Convert.ToInt32(mySqlDataReader["ArticoloID"]);
+                    //myArticolo.Nome = Convert.ToString(mySqlDataReader["Nome"]);
+                    //myArticolo.Descrizione = Convert.ToString(mySqlDataReader["Descrizione"]);
+                    //myArticolo.PrezzoUnitarioVendita = Convert.ToSingle(mySqlDataReader["PrezzoUnitarioVendita"]);
+                    //myArticolo.Giacenza = Convert.ToInt32(mySqlDataReader["Giacenza"]);
+                    //myArticolo.AliquotaIVA = Convert.ToInt32(mySqlDataReader["AliquotaIVA"]);
+
+                    Articolo myArticolo = new Articolo
+                    {
+                        ArticoloID = Convert.ToInt32(mySqlDataReader["ArticoloID"]),
+                        Nome = Convert.ToString(mySqlDataReader["Nome"]),
+                        Descrizione = Convert.ToString(mySqlDataReader["Descrizione"]),
+                        PrezzoUnitarioVendita = Convert.ToSingle(mySqlDataReader["PrezzoUnitarioVendita"]),
+                        Giacenza = Convert.ToInt32(mySqlDataReader["Giacenza"]),
+                        AliquotaIVA = Convert.ToInt32(mySqlDataReader["AliquotaIVA"])
+                    };
+
+                    // myArticolo puo essere aggiunto alla lista degli articoli
+                    myListArticoli.Add(myArticolo);
+                }
+
+            }
+            catch (Exception exc)
+            {
+                // in caso di errore dovremo comunicare al frontend
+                // tramite un'altro metodo
+            }
+            finally
+            {
+                // che tutto sia OK oppure KO il codice dentro finally verra sempre eseguita x.y
+                // chiudo la connessione al db
+                mySqlConnection.Close();
+            }
+
+            // fine della funzione GetListArticoli
+
+            return myListArticoli;
+        }
+
+        [Route("articolo")]
+        [HttpGet]
+        public Articolo GetArticolo(Int32 ArticoloID)
+        {
+            // tutto il codice che legge 1 solo articolo
+            // utilizziamo 1 parametro
+            // SELECT * TArticoli WHERE ArticoloID=@ArticoloID
+
+            SqlConnection mySqlConnection = null;
+
+            // dichiaro una List di Articoli
+
+            Articolo mArticolo = new Articolo();
+
+            try
+            {
+                // per connettermi al db
+                // mi serve una stringa di connessione
+                String stringaConnessione = "Server=localhost\\SQLEXPRESS; Database=DBGestionaleVI; Trusted_Connection=True";
+
+                // creo l'istanza della connessione al DB
+                mySqlConnection = new SqlConnection(stringaConnessione);
+
+                // provo aprire la connessione al DB
+                mySqlConnection.Open();
+
+                // creo l'oggetto che mi permette di estrarre i dati da TArticoli
+                SqlCommand mySqlCommand = new SqlCommand();
+                mySqlCommand.Connection = mySqlConnection;
+
+                mySqlCommand.Parameters.Add("@ID", SqlDbType.Int);
+                mySqlCommand.Parameters["@ID"].Value = ArticoloID;
+
+                mySqlCommand.CommandText = $"SELECT * FROM TArticoli WHERE ArticoloID=@ID";
+
+                // devo eseguire il mySqlCommand e ottengo un SqlDataReader
+                SqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
+
+                // adesso il mySqlDataReader contiene i vari record
+                // che andranno copiati dentro myListArticoli
+                
+                if (mySqlDataReader.Read())
+                {
+                    mArticolo.ArticoloID = Convert.ToInt32(mySqlDataReader["ArticoloID"]);
+                    mArticolo.Nome = Convert.ToString(mySqlDataReader["Nome"]);
+                    mArticolo.Descrizione = Convert.ToString(mySqlDataReader["Descrizione"]);
+                    mArticolo.PrezzoUnitarioVendita = Convert.ToSingle(mySqlDataReader["PrezzoUnitarioVendita"]);
+                    mArticolo.Giacenza = Convert.ToInt32(mySqlDataReader["Giacenza"]);
+                    mArticolo.AliquotaIVA = Convert.ToInt32(mySqlDataReader["AliquotaIVA"]);   
+                }
+
+            }
+            catch (Exception exc)
+            {
+                // in caso di errore dovremo comunicare al frontend
+                // tramite un'altro metodo
+            }
+            finally
+            {
+                // che tutto sia OK oppure KO il codice dentro finally verra sempre eseguita x.y
+                // chiudo la connessione al db
+                mySqlConnection.Close();
+            }
+
+            // fine della funzione GetListArticoli
+            return mArticolo;
+            // return di myArticolo
+        }
+
+        // seconda chiamata - POST - che riceve un articolo
+        // e lo aggiunge alla TArticoli
+    }
+}
